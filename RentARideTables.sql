4ea8aac9e9785b05fca276cc9ce40af4eac9058c
kathsoll <kbs24766@uga.edu>
2017-10-06 12:41:05 -0400

    Enums fixes
    It should run now :)

--- RentARideTables.sql
+++ RentARideTables.sql
@@ -1,4 +1,4 @@
---drop existing tables if they exist
+/*drop tables if they exist*/
 DROP TABLE IF EXISTS RentalLocation;
 DROP TABLE IF EXISTS Vehicle;
 DROP TABLE IF EXISTS VehicleType;
@@ -14,13 +14,13 @@ DROP TABLE IF EXISTS VehicleStatus;
 DROP TABLE IF EXISTS VehicleCondition;
 DROP TABLE IF EXISTS UserStatus;
 
---create ENUM tables
+/*create ENUM tables*/
 CREATE TABLE VehicleStatus ( vs ENUM('INLOCATION', 'INRENTAL') );
 CREATE TABLE VehicleCondition ( vc ENUM('GOOD', 'NEEDSMAINTENANCE') );
 CREATE TABLE UserStatus ( us ENUM('ACTIVE', 'CANCELED', 'TERMINATED') );
 
 
---create table for RentalLocation 
+/*create table for RentalLocation */
 CREATE TABLE RentalLocation (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL,
@@ -28,20 +28,20 @@ address VARCHAR(255) NOT NULL,
 capacity INT UNSIGNED NOT NULL
 );
 
---create table for HourlyPrice
+/*create table for HourlyPrice*/
 CREATE TABLE HourlyPrice(
 maxHrs INT UNSIGNED NOT NULL,
 price INT UNSIGNED NOT NULL
 );
 
---create table for VehicleType
+/*create table for VehicleType*/
 CREATE TABLE VehicleType (
 name VARCHAR(255) NOT NULL,
 price INT UNSIGNED NOT NULL,
 FOREIGN KEY (price) REFERENCES HourlyPrice(price)
 );
 
---create table for Vehicle
+/*create table for Vehicle*/
 CREATE TABLE Vehicle (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 locationID INT UNSIGNED NOT NULL,
@@ -51,15 +51,17 @@ vehicleYear INT UNSIGNED NOT NULL,
 mileage INT UNSIGNED NOT NULL,
 tag VARCHAR(255) NOT NULL,
 lastServiced DATETIME NOT NULL,
-status AS SELECT vs AS s FROM VehicleStatus,
-condition AS SELECT vc AS c FROM VehicleCondition,
+status ENUM('INLOCATION', 'INRENTAL'),
+vehicleCondition ENUM('GOOD', 'NEEDSMAINTENANCE'),
 vehicleType VARCHAR(255) NOT NULL,
+FOREIGN KEY (status) REFERENCES VehicleStatus(vs),
+FOREIGN KEY (vehicleCondition) REFERENCES VehicleCondition(vc),
 FOREIGN KEY (vehicleType) REFERENCES VehicleType(name),
 FOREIGN KEY (locationID) REFERENCES RentalLocation(id)
 );
 
 
---create table for Users
+/*create table for Users*/
 CREATE TABLE Users(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 firstName VARCHAR(255) NOT NULL,
@@ -71,7 +73,7 @@ address VARCHAR(255) NOT NULL,
 createdDate DATETIME NOT NULL
 );
 
---create table for Rental
+/*create table for Rental*/
 CREATE TABLE Rental(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 pickupTime DATETIME NOT NULL,
@@ -87,7 +89,7 @@ FOREIGN KEY (vehicleID) REFERENCES Vehicle(id),
 FOREIGN KEY (customerID) REFERENCES Customer(id)
 );
 
---create table for Reservation
+/*create table for Reservation*/
 CREATE TABLE Reservation(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 pickupTime DATETIME NOT NULL,
@@ -100,13 +102,13 @@ FOREIGN KEY (customerID) REFERENCES Customer(id),
 FOREIGN KEY (locationID) REFERENCES RentalLocation(id)
 );
 
---create table for Administrator
+/*create table for Administrator*/
 CREATE TABLE Administrator(
 id INT UNSIGNED NOT NULL,
 FOREIGN KEY (id) REFERENCES Users(id)
 );
 
---create table for Customer
+/*create table for Customer*/
 CREATE TABLE Customer(
 id INT UNSIGNED NOT NULL,
 memberUntil DATETIME NOT NULL,
@@ -114,19 +116,20 @@ licState VARCHAR(255) NOT NULL,
 licNumber VARCHAR(255) NOT NULL,
 ccNumber VARCHAR(255) NOT NULL,
 ccExpiration DATETIME NOT NULL,
-status AS SELECT us AS cs FROM UserStatus,
-FOREIGN KEY (id) REFERENCES Users(id)
+status ENUM('ACTIVE', 'CANCELED', 'TERMINATED'),
+FOREIGN KEY (id) REFERENCES Users(id),
+FOREIGN KEY (status) REFERENCES UserStatus(us)
 );
 
---create table for Comments
+/*create table for Comments*/
 CREATE TABLE Comments(
 text VARCHAR(255) NOT NULL,
-commentDate DATETIME NOT NULL
-customerID VARCHAR(255) NOT NULL,
+commentDate DATETIME NOT NULL,
+customerID INT UNSIGNED NOT NULL,
 FOREIGN KEY (customerID) REFERENCES Customer(id)
 );
 
---create table for RentARideParams
+/*create table for RentARideParams*/
 CREATE TABLE RentARideParams(
 membershipPrice INT UNSIGNED NOT NULL,
 lateFee INT UNSIGNED NOT NULL
